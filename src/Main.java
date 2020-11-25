5871edf3f218f471f601dfd3ff53b51d6265c214
BK201 <bk201.dev@gmail.bg>
2020-11-25 10:06:04 +0100

    Added some Panels into the JFrame,bottom Buttons and some Labels.

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,19 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         JFrame mainFrame = new JFrame("Tic Tac Toe with Swing Library");
+        JPanel gridPanel = new JPanel();
+        JPanel bottomButtons = new JPanel();
+        JPanel topElements = new JPanel();
 
-        GridLayout layout = new GridLayout(3,3);
-        mainFrame.setLayout(layout);
+        mainFrame.setLayout(new BorderLayout());
+        gridPanel.setLayout(new GridLayout(3,3));
+        bottomButtons.setLayout(new FlowLayout());
+        topElements.setLayout(new FlowLayout());
 
         // Defining the Frame and it's basic options
-        mainFrame.setSize(500,500);
+        mainFrame.setSize(400,400);
         mainFrame.setVisible(true);
+        mainFrame.setLocation(450,100);
         mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
 
@@ -24,15 +30,38 @@ public class Main {
         JButton jb7 = new JButton();
         JButton jb8 = new JButton();
         JButton jb9 = new JButton();
+        JButton play = new JButton("Play");
+        JButton newgame = new JButton("New Game");
 
-        mainFrame.add(jb1);
-        mainFrame.add(jb2);
-        mainFrame.add(jb3);
-        mainFrame.add(jb4);
-        mainFrame.add(jb5);
-        mainFrame.add(jb6);
-        mainFrame.add(jb7);
-        mainFrame.add(jb8);
-        mainFrame.add(jb9);
+        gridPanel.add(jb1);
+        gridPanel.add(jb2);
+        gridPanel.add(jb3);
+        gridPanel.add(jb4);
+        gridPanel.add(jb5);
+        gridPanel.add(jb6);
+        gridPanel.add(jb7);
+        gridPanel.add(jb8);
+        gridPanel.add(jb9);
+        bottomButtons.add(play);
+        bottomButtons.add(newgame);
+
+        mainFrame.add(bottomButtons,BorderLayout.PAGE_END);
+        mainFrame.add(gridPanel,BorderLayout.CENTER);
+        mainFrame.add(topElements,BorderLayout.PAGE_START);
+
+
+        //Defining Labels
+        JLabel victory = new JLabel("hat gewonnen !!!");
+        JLabel player = new JLabel("");
+        victory.setFont(new java.awt.Font("Arial",Font.BOLD,20));
+
+        topElements.add(player);
+        topElements.add(victory);
+
+
+
+        //Game Mechanics
+        boolean randomPlayer;
+
     }
 }
